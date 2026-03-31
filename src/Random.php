40e415ee8ce94c84b217f089529cfbc26fc42caf
Robert Böser <code@robertboeser.de>
2026-03-31 02:55:40 +0200

    Optimized namespace

--- src/Random.php
+++ src/Random.php
@@ -1,63 +1,103 @@
 <?php
-namespace Robo\RoboRnd;
 
-class Random {
-    const NUMBER = '0123456789';
-    const ALPHA  = 'abcdefghijklmnopqrstuvwxyz';
-    const CROCKFORD = 'ABCDEFGHJKMNPQRSTVWXYZ';
-    const MODHEX = 'cbdefghijklnrtuv';
-    const MITTWALD = '$!#%*+,-.;_:/';
-    const ANYCHAR = '~!@#$%^&*()-_=+[]{}|;:,./<>?';
+namespace Robo\Rnd;
 
-    function buildAlphabet($type, $lohi=0) {
-        switch($type) {
+class Random
+{
+    public const NUMBER = '0123456789';
+    public const ALPHA  = 'abcdefghijklmnopqrstuvwxyz';
+    public const CROCKFORD = 'ABCDEFGHJKMNPQRSTVWXYZ';
+    public const MODHEX = 'cbdefghijklnrtuv';
+    public const MITTWALD = '$!#%*+,-.;_:/';
+    public const ANYCHAR = '~!@#$%^&*()-_=+[]{}|;:,./<>?';
+
+    public function buildAlphabet($type, $lohi = 0)
+    {
+        switch ($type) {
             case 'num':
                 return self::NUMBER;
             case 'alpha':
                 $alp = '';
-                if($lohi & 2) $alp .= strtoupper(self::ALPHA);
-                if($lohi & 1) $alp .= self::ALPHA;
-                if(!$lohi) $alp .= self::ALPHA;
+                if ($lohi & 2) {
+                    $alp .= strtoupper(self::ALPHA);
+                }
+                if ($lohi & 1) {
+                    $alp .= self::ALPHA;
+                }
+                if (!$lohi) {
+                    $alp .= self::ALPHA;
+                }
                 return $alp;
             case 'crockford':
                 $alp = self::NUMBER;
-                if($lohi & 2) $alp .= self::CROCKFORD;
-                if($lohi & 1) $alp .= strtolower(self::CROCKFORD);
-                if(!$lohi) $alp .= self::CROCKFORD;
+                if ($lohi & 2) {
+                    $alp .= self::CROCKFORD;
+                }
+                if ($lohi & 1) {
+                    $alp .= strtolower(self::CROCKFORD);
+                }
+                if (!$lohi) {
+                    $alp .= self::CROCKFORD;
+                }
                 return $alp;
             case 'modhex':
                 $alp = '';
-                if($lohi & 2) $alp .= strtoupper(self::MODHEX);
-                if($lohi & 1) $alp .= self::MODHEX;
-                if(!$lohi) $alp .= self::MODHEX;
+                if ($lohi & 2) {
+                    $alp .= strtoupper(self::MODHEX);
+                }
+                if ($lohi & 1) {
+                    $alp .= self::MODHEX;
+                }
+                if (!$lohi) {
+                    $alp .= self::MODHEX;
+                }
                 return $alp;
             case 'mittwald':
                 $alp = self::NUMBER . self::MITTWALD;
-                if($lohi & 2) $alp .= strtoupper(self::ALPHA);
-                if($lohi & 1) $alp .= self::ALPHA;
-                if(!$lohi) $alp .= self::ALPHA;
+                if ($lohi & 2) {
+                    $alp .= strtoupper(self::ALPHA);
+                }
+                if ($lohi & 1) {
+                    $alp .= self::ALPHA;
+                }
+                if (!$lohi) {
+                    $alp .= self::ALPHA;
+                }
                 return $alp;
             case 'anychar':
                 $alp = self::NUMBER . self::ANYCHAR;
-                if($lohi & 2) $alp .= strtoupper(self::ALPHA);
-                if($lohi & 1) $alp .= self::ALPHA;
-                if(!$lohi) $alp .= self::ALPHA;
+                if ($lohi & 2) {
+                    $alp .= strtoupper(self::ALPHA);
+                }
+                if ($lohi & 1) {
+                    $alp .= self::ALPHA;
+                }
+                if (!$lohi) {
+                    $alp .= self::ALPHA;
+                }
                 return $alp;
             case 'alnum':
             default:
                 $alp = self::NUMBER;
-                if($lohi & 2) $alp .= strtoupper(self::ALPHA);
-                if($lohi & 1) $alp .= self::ALPHA;
-                if(!$lohi) $alp .= self::ALPHA;
-                return $alp;    
+                if ($lohi & 2) {
+                    $alp .= strtoupper(self::ALPHA);
+                }
+                if ($lohi & 1) {
+                    $alp .= self::ALPHA;
+                }
+                if (!$lohi) {
+                    $alp .= self::ALPHA;
+                }
+                return $alp;
         }
     }
 
-    function random($len, $type='', $lohi=0) {
+    public function random($len, $type = '', $lohi = 0)
+    {
         $alp = $this->buildAlphabet($type, $lohi);
         $rnd = '';
-        for($i = 0; $i < $len; $i++) {
-            $x = random_int(0, strlen($alp)-1);
+        for ($i = 0; $i < $len; $i++) {
+            $x = random_int(0, strlen($alp) - 1);
             $rnd .= $alp[$x];
         }
         return $rnd;
